1d7c60260722e62f7f87bdf9e3fb688155b0ff35
Hehelskyi <viktor.hehelskyi@ning.com>
2023-08-10 19:34:09 +0300

    execTpl func

--- main.go
+++ main.go
@@ -2,54 +2,57 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-// func route(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/dashboard":
-// 		handlerDash(w, r)
-// 	case "/":
-// 		defaultHandler(w)
-// 	default:
-// 		http.Error(w, "PAGE NOT FOUND", http.StatusNotFound)
-// 		// w.WriteHeader(http.StatusNotFound)
-// 		// fmt.Fprint(w, "<h1>Page not found!!!</h1>")
-// 	}
-// }
-
-func defaultHandler(w http.ResponseWriter) {
+func execTpl(w http.ResponseWriter, tpl string) bool {
 	w.Header().Set("content-type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome12312312!!</h1>")
+	t, err := template.ParseFiles(tpl)
+	if err != nil {
+		log.Printf("error parsing template %v", err)
+		http.Error(w, "error parsing template", http.StatusInternalServerError)
+		return true
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Printf("error executing template %v", err)
+		http.Error(w, "error executing template", http.StatusInternalServerError)
+		return true
+	}
+	return false
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	execTpl(w, filepath.Join("templates", "hello.gohtml"))
 }
 
 func handlerDash(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	w.Header().Set("content-type", "text/html")
-	fmt.Fprint(w, "<pre>"+path+"</pre>")
+	execTpl(w, filepath.Join("templates", "dashboard.gohtml"))
 }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/dashboard":
-		handlerDash(w, r)
-	case "/":
-		defaultHandler(w)
-	default:
-		http.Error(w, "PAGE NOT FOUND", http.StatusNotFound)
-		// w.WriteHeader(http.StatusNotFound)
-		// fmt.Fprint(w, "<h1>Page not found!!!</h1>")
-	}
+func handleCustom(w http.ResponseWriter, r *http.Request) {
+	execTpl(w, filepath.Join("templates", "custom.gohtml"))
 }
 
 func main() {
-	// mux := http.NewServeMux()
-	// mux.HandleFunc()
-	// http.HandleFunc("/", route)
-	var router Router
+	router := chi.NewRouter()
+	// router.Use(middleware.Logger)
+	router.Get("/", handleRoot)
+	router.Get("/dashboard", handlerDash)
+	router.Route("/photos", func(r chi.Router) {
+		r.Use(middleware.Logger)
+		r.Get("/{photoSlug:[a-zA-z-0-9]+}", handleCustom)
+	})
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "PAGE NOT FOUND", http.StatusNotFound)
+	})
 	time.Sleep(1 * time.Second)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", router)
